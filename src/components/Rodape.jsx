b9b2386d8ad5c6c0b9c1ad823b00c729db73b4cf
cauamongs <rm552178@fiap.com.br>
2023-08-24 12:02:41 -0300

    Alterando app.jsx e adicionando cabecalho, rodape e conteudo.jsx

---
+++ src/components/Rodape.jsx
@@ -0,0 +1,19 @@
+ 
+export default function Rodape(){
+
+    return(
+        <>
+        
+        <footer>
+      <div>
+        <ul>
+          <li><a href="#">Git Hub</a></li>
+          <li><a href="#"></a>X</li>
+          <li><a href="#"></a>Reddit</li>
+        </ul>
+      </div>
+    </footer>
+        
+        </>
+    );
+}
