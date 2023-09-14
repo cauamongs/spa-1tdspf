526dfe0f472adb5a678d1f0d1d231e131f2563f8
cauamongs <rm552178@fiap.com.br>
2023-09-14 11:19:13 -0300

    Estilizando ExcluirProduto

--- src/routes/Produtos.jsx
+++ src/routes/Produtos.jsx
@@ -3,8 +3,20 @@ import { ListaProdutos } from "../components/ListaProdutos";
 import {AiFillEdit as EditObj} from "react-icons/ai"
 import {RiDeleteBin2Fill as DelObj} from "react-icons/ri";
 import estilos from "./Produtos.module.css";
+import ModalData from "../components/ModalData";
+import {useState} from "react";
 
 export default function Produtos() {
+
+ const [status, setStatus] = useState({
+  open: false,
+  id:0,
+ })
+
+ const handleStatus = (status,id)=>{
+    setStatus({"open":status,"id":id})
+ }
+  
   return (
     <>
       <h1>Produtos Informáticos - FIAPO</h1>
@@ -16,6 +28,7 @@ export default function Produtos() {
             <th>NOME</th>
             <th>DESCRIÇÃO</th>
             <th>PREÇO</th>
+            <th>IMAGEM</th>
             <th>EDITAR/EXCLUIR</th>
           </tr>
         </thead>
@@ -27,16 +40,23 @@ export default function Produtos() {
               <td>{produto.nome}</td>
               <td>{produto.desc}</td>
               <td>{produto.preco}</td>
+              <td><img src={produto.img} alt={produto.desc} /></td>
               <td> <Link to={`/editar/produtos/${produto.id}`}><EditObj/></Link> | <Link to={`/excluir/produtos/${produto.id}`}><DelObj/></Link></td>
+
             </tr>
           ))}
         </tbody>
         <tfoot>
           <tr>
-            <td colSpan={5}>PRODUTOS</td>
+            <td colSpan={6}>PRODUTOS</td>
           </tr>
         </tfoot>
       </table>
+
+            <div>
+              <ModalData statusModal={status.open} idProduto={status.id}/>
+            </div>
+
     </>
   );
 }
